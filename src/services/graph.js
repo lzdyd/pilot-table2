946a10feb0c6c7b731d5bd7ca380bb6ac7dcd417
Jamik Karimov <jkarimov1593@gmail.com>
2017-10-26 00:25:47 +0300

    authentication complete 50%

--- src/services/graph.js
+++ src/services/graph.js
@@ -1,33 +1,33 @@
 export default class Graph {
-    constructor() {
-        this.nodes = [];
+  constructor() {
+    this.nodes = [];
+  }
+
+  addNode(key, value, label, state, formula) {
+    this.nodes.push({
+      key,
+      value,
+      label,
+      state,
+      formula,
+      dependence: []
+    });
+  }
+
+  find(key) {
+    return this.nodes.find((node) => {
+      return node.key === key;
+    });
+  }
+
+  addLine(startValue, endValue) {
+    const startNode = this.find(startValue);
+    const endNode = this.find(endValue);
+
+    if (!startNode || !endNode) {
+      throw new Error('Обе вершины должны существовать');
     }
 
-    addNode(key, value, label, state, formula) {
-        this.nodes.push({
-            key,
-            value,
-            label,
-            state,
-            formula,
-            dependence: []
-        });
-    }
-
-    find(key) {
-        return this.nodes.find(function(node) {
-            return node.key === key;
-        });
-    }
-
-    addLine(startValue, endValue) {
-        let startNode = this.find(startValue);
-        let endNode = this.find(endValue);
-
-        if (!startNode || !endNode) {
-            throw new Error('Обе вершины должны существовать');
-        }
-
-        startNode.dependence.push(endNode);
-    }
+    startNode.dependence.push(endNode);
+  }
 }
